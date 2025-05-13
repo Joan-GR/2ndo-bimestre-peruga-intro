fb53e6fa80864f427567a61efdc487faafb6a0a9
Joan-GR <joanguerson@gmail.com>
2025-05-13 08:52:13 -0300

    UI del TP

---
+++ Assets/SumaYResultado.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;    
+
+public class SumaYResultado : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public TMP_InputField ImputNum1;
+    public TMP_InputField ImputNum2;
+    public TextMeshProUGUI TxtResultado;
+    int num1;
+    int num2;
+    void Start()
+    {
+        TxtResultado.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SumarYMostrar()
+    {
+
+    }
+}
